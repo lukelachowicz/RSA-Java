7553ef98309454e966b6728f88c5c93e85e5f3d0
Pial Das <daspia85@students.rowan.edu>
2023-11-13 20:32:25 -0500

    Added method stubs for the RSA class. Return statements are bogus.

---
+++ RSA.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class RSA {
+	public static long inverse(long e, long m) {
+		return 0;
+	}
+
+	public static void show(long[] cipher) {
+	}
+
+	public static long modPower(long b, long p, long m) {
+		return 0;
+	}
+
+	public static long randPrime(int m, int n, Random rand) {
+		return 0;
+	}
+
+	public static long relPrime(long n, Random rand) {
+		return 0;
+	}
+
+	public static long toLong(String msg, int p) {
+		return 0;
+	}
+
+	public static String longTo2Chars(long x) {
+		return "";
+	}
+}

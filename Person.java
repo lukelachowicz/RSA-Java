e8a3be067484296baffda2b74fa3ddcc232b9eed
Pial Das <daspia85@students.rowan.edu>
2023-11-13 20:31:52 -0500

    Added method stubs for the Person class. Return statements are bogus.

---
+++ Person.java
@@ -0,0 +1,20 @@
+public class Person {
+	public Person() {
+	}
+
+	public long getM() {
+		return 0;
+	}
+
+	public long getE() {
+		return 0;
+	}
+
+	public long[] encryptTo(String msg) {
+		return new long[8];
+	}
+
+	public String decrypt(long[] cipher) {
+		return "";
+	}
+}

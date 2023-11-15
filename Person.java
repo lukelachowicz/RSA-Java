2de7058ba396be64ecf3b7e302aeeceb77976bbf
Pial Das <daspia85@students.rowan.edu>
2023-11-15 15:04:13 -0500

    Implemented constructor and class fields.

--- Person.java
+++ Person.java
@@ -1,5 +1,12 @@
 public class Person {
-	public Person() {
+	private long modulus;  // The public modulus 'm'
+	private long pub_exp;  // The public exponent 'e'
+	private long priv_exp;  // The private exponent 'd' aka the key
+
+	public Person(long modulus, long pub_exp, long priv_exp) {
+		this.modulus = modulus;
+		this.pub_exp = pub_exp;
+		this.priv_exp = priv_exp;
 	}
 
 	public long getM() {

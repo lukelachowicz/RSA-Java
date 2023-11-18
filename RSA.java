b5ff67bdab435de99a7e97912eb274f0de3e1a6c
Pial Das <daspia85@students.rowan.edu>
2023-11-17 19:37:41 -0500

    Changed class template. RSA no longer uses static methods and does not have a main method anymore. Separate driver class is required for testing.

--- RSA.java
+++ RSA.java
@@ -1,33 +1,30 @@
 import java.util.Random;
 
 public class RSA {
-	public static long inverse(long e, long m) {
+	public long inverse(long e, long m) {
 		return 0;
 	}
 
-	public static void show(long[] cipher) {
+	public void show(long[] cipher) {
 	}
 
-	public static long modPower(long b, long p, long m) {
+	public long modPower(long b, long p, long m) {
 		return 0;
 	}
 
-	public static long randPrime(int m, int n, Random rand) {
+	public long randPrime(int m, int n, Random rand) {
 		return 0;
 	}
 
-	public static long relPrime(long n, Random rand) {
+	public long relPrime(long n, Random rand) {
 		return 0;
 	}
 
-	public static long toLong(String msg, int p) {
+	public long toLong(String msg, int p) {
 		return 0;
 	}
 
-	public static String longTo2Chars(long x) {
+	public String longTo2Chars(long x) {
 		return "";
-	}
-
-	public static void main(String[] args) {
 	}
 }
